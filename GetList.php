7a54f43c2ae75063e267f21ba3119a2562140f32
IT007boy <570442604@qq.com>
2016-09-24 12:48:23 +0800

    update

---
+++ GetList.php
@@ -0,0 +1,25 @@
+<?php 
+header("Content-Type:application/json;charset=utf-8"); 
+echo('{"d": [ 
+{"img": "images/flow-box/1.jpg", "title": "图片1"}, 
+{"img": "images/flow-box/2.jpg", "title": "图片2"}, 
+{"img": "images/flow-box/3.jpg", "title": "图片3"}, 
+{"img": "images/flow-box/4.jpg", "title": "图片4"}, 
+{"img": "images/flow-box/5.jpg", "title": "图片5"}, 
+{"img": "images/flow-box/6.jpg", "title": "图片6"}, 
+{"img": "images/flow-box/7.jpg", "title": "图片7"}, 
+{"img": "images/flow-box/8.jpg", "title": "图片8"}, 
+{"img": "images/flow-box/9.jpg", "title": "图片9"}, 
+{"img": "images/flow-box/10.jpg", "title": "图片10"}, 
+{"img": "images/flow-box/11.jpg", "title": "图片11"}, 
+{"img": "images/flow-box/12.jpg", "title": "图片12"}, 
+{"img": "images/flow-box/13.jpg", "title": "图片13"}, 
+{"img": "images/flow-box/14.jpg", "title": "图片14"}, 
+{"img": "images/flow-box/15.jpg", "title": "图片15"}, 
+{"img": "images/flow-box/16.jpg", "title": "图片16"}, 
+{"img": "images/flow-box/17.jpg", "title": "图片17"}, 
+{"img": "images/flow-box/18.jpg", "title": "图片18"}, 
+{"img": "images/flow-box/19.jpg", "title": "图片19"}, 
+{"img": "images/flow-box/20.jpg", "title": "图片20"} 
+]}'); 
+?>
