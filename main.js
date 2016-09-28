70dd867761c338bb740e74df978c171f4612257b
IT007boy <570442604@qq.com>
2016-09-28 20:31:19 +0800

    dd

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ function cata (){
 	var catimagebox=document.getElementById('catimagebox');//得到对应的id
 	if (zero==mycat.length-1) {zero=0;catimage.src=mycat[zero];}
 		else{zero+=1;catimage.src=mycat[zero];
-			if(zero<=6){if (imageLeft>=document.body.clientWidth) {imageLeft=0;catimagebox.style.paddingLeft=0+"px";}imageLeft+=Math.random()*80;catimagebox.style.paddingLeft=imageLeft+"px";}//随机数控制距离和margin控制方向,判断是否超过界限并重置
+			if(zero<=6){if (imageLeft>=document.body.clientWidth) {imageLeft=0;catimagebox.style.paddingLeft=0+"px";}imageLeft+=Math.random()*40;catimagebox.style.paddingLeft=imageLeft+"px";}//随机数控制距离和margin控制方向,判断是否超过界限并重置
 			if(zero<=12&&zero>6){imageTop-=Math.random()*10;catimagebox.style.paddingTop=imageTop+"px";}
-			if(zero<=18&&zero>12){if(imageTop>=document.body.clientHeight){imageTop=0;catimagebox.style.paddingTop=0+"px";}imageTop+=Math.random()*80;catimagebox.style.paddingTop=imageTop+"px";}
+			if(zero<=18&&zero>12){if(imageTop>=document.body.clientHeight){imageTop=0;catimagebox.style.paddingTop=0+"px";}imageTop+=Math.random()*40;catimagebox.style.paddingTop=imageTop+"px";}
 		}
 }
 setInterval("cata()",200);//控制时间
